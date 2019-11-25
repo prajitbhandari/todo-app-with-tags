ccf040746290795d4b6a7d2bf5bc4ff91e77343c
prajitbhandari <prajitbhandari@gmail.com>
2019-11-25 17:44:04 +0545

    Rspec with error message

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe Tag, type: :model do
   context "when tag is to be created" do
     it 'is not valid without tag name' do
       tag = Tag.new(name: nil)
-      expect(tag).not_to be_nil
+      tag.valid?
+      expect(tag.errors[:name]).to include("can't be blank")
     end
     it "is valid with tag name" do
       tag = Tag.new(name: "Enter some tag name")
       tag.save
-      expect(tag).to be_valid
+      expect(tag).to be_valid, "Tag name is valid"
     end
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,17 +4,14 @@ RSpec.describe Todo, type: :model do
   context "when todo is to be created" do
     it "is not valid without todo item " do
       todo = Todo.new(item: nil)
-      expect(todo).not_to be_nil
+      todo.save
+      expect(todo.errors.keys).to include(:item)
+      expect(todo.errors.full_messages).to eq(["Item can't be blank"])
     end
     it "is valid to be created" do
       todo = Todo.new(item: "Some text for todo item")
       todo.save
-      expect(todo).to be_valid
+      expect(todo).to be_valid, "Todo item is valid"
     end
   end
 end
-
-
-
-
-
