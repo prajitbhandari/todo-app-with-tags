a184ccfde2a0672999d18b30da92e2486cfd2f76
prajitbhandari <prajitbhandari@gmail.com>
2020-01-06 12:26:07 +0545

    Feature Testing Done

--- spec/features/soft_delete_spec.rb
+++ spec/features/soft_delete_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
+require 'byebug'
 RSpec.describe 'Soft Delete Todo Item', js: true do
   it 'with todo Destroy' do
     visit 'http://localhost:3000'
-    find('#delete11').click
-    alertPop = page.driver.browser.switch_to.alert
-    sleep 5
-    alertPop.accept  # can also be a.dismiss
-    sleep 5
+    #debugger
+    itemCount = page.all(".markItem").size
+    getId = 1 + rand(itemCount)
+    if page.has_css?("#delete"+ getId.to_s, wait: 0)
+      find(:css, "#delete" + getId.to_s).click
+      alertPop = page.driver.browser.switch_to.alert
+      sleep 5
+      alertPop.accept  # can also be alertPop.dismiss
+      sleep 5
+    else
+      find(:css, "#recover" + getId.to_s).click
+      sleep 5
+      alertPop = page.driver.browser.switch_to.alert
+      sleep 5
+      alertPop.accept
+      sleep 5
+    end
   end
 end
 
