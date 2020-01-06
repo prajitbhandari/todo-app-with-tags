5a71c681bf7d5e729a3570136b176a8e629cf688
prajitbhandari <prajitbhandari@gmail.com>
2020-01-06 12:40:48 +0545

    Feature Testing done

--- spec/features/soft_delete_spec.rb
+++ spec/features/soft_delete_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Soft Delete Todo Item', js: true do
     #debugger
     itemCount = page.all(".markItem").size
     getId = 1 + rand(itemCount)
+    recover_or_purge_id = ["#recover", "#purge"].sample
     if page.has_css?("#delete"+ getId.to_s, wait: 0)
       find(:css, "#delete" + getId.to_s).click
       alertPop = page.driver.browser.switch_to.alert
@@ -13,7 +14,7 @@ RSpec.describe 'Soft Delete Todo Item', js: true do
       alertPop.accept  # can also be alertPop.dismiss
       sleep 5
     else
-      find(:css, "#recover" + getId.to_s).click
+      find(:css, recover_or_purge_id + getId.to_s).click
       sleep 5
       alertPop = page.driver.browser.switch_to.alert
       sleep 5
