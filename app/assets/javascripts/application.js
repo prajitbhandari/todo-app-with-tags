70a952113b8dfe7952d2481739ac4d2d832e3463
prajitbhandari <prajitbhandari@gmail.com>
2020-01-13 16:13:04 +0545

    swal introduced

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require select2
 //= require sweetalert2
 //= require sweet-alert2-rails
 //= require_tree .
+
+
